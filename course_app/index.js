04da0c0e9ad5f1978e68ca31d13502f1537c3335
dzemildupljak <dzemildupljak4795@gmail.com>
2022-12-06 19:33:22 +0100

    test delete stuff

--- course_app/index.js
+++
@@ -1,9 +0,0 @@
-async function getMockData() {
-  try {
-    const response = await axios.get("http://localhost:3000/mock-data");
-
-    console.log(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-}
