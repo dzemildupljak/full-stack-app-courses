64536036bc6f2630357d00bc5d267f13d0c6a970
dzemildupljak <dzemildupljak4795@gmail.com>
2022-12-06 19:20:37 +0100

    initial commit full stack app

---
+++ course_api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const port = 3000;
+
+app.get("/mock-data", (_, res) => {
+  const mockData = [
+    {
+      lessonName: "Lekcija 1",
+      description: "Opis lekcije 1",
+    },
+    {
+      lessonName: "Lekcija 2",
+      description: "Opis lekcije 2",
+    },
+  ];
+
+  res.send({
+    data: mockData,
+    message: "list of lessons",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
