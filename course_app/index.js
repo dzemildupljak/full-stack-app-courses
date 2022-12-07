cd59eaf884170466acc90d96633df7fa52e8ef53
kemalkujovic <kemalkujovic1111@gmail.com>
2022-12-07 09:15:26 +0100

    login-frontend

--- course_app/index.js
+++ course_app/index.js
@@ -1,10 +1,42 @@
+const loginBtn = document.querySelector("#loginBtn");
+const closeBtn = document.querySelector("#closeModal");
+const regBtn = document.querySelector("#regBtn");
+const modal = document.querySelector(".custom-modal");
+const register = document.querySelector(".registerBtn");
+const BASE_URL = "http://localhost:3000/mock-data";
 async function getMockData() {
   try {
-    const response = await axios.get("http://localhost:3000/mock-data");
-
+    const response = await axios.get(BASE_URL);
     console.log(response.data);
-    console.log("NOVA GRANA");
   } catch (error) {
     console.log(error);
   }
 }
+loginBtn.addEventListener("click", (e) => {
+  console.log("radi");
+  e.preventDefault();
+  loginAcces();
+});
+async function loginAcces() {
+  let email = document.querySelector("#login_email");
+  let pass = document.querySelector("#login_password");
+  console.log(email.value);
+  try {
+    let response = await axios.post(BASE_URL, {
+      email: email.value,
+      password: pass.value,
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// regBtn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   modal.style.display = "block";
+// });
+// closeBtn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   modal.style.display = "none";
+// });
